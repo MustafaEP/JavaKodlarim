377abce6379b7c6a183bceb098440b04c8589ad2
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-04-09 21:18:49 +0300

    1-DaireninAlani

---
+++ 1-DaireninAlani/src/pkg1/daireninalani/DaireninAlani.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1.daireninalani;
+
+/**
+ *
+ * @author porta
+ */
+public class DaireninAlani {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double yaricap;
+        double alan;
+        
+        //yarıçapı atıyoruz
+        yaricap = 20;
+        
+        //Alanı hesaplayalım
+        alan = yaricap * yaricap * 3.14159;
+        
+        //Ekran Sonucunu Yazdıralım
+        System.out.println("Yaricap: " + yaricap);
+        System.out.println("Alan: " + alan);
+        
+        
+        
+    }
+    
+}
