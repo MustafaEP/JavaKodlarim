f936e048b4bd7ef83d06e9ed22c6bade59d68645
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-04-09 21:19:23 +0300

    2-DialogKutusu

---
+++ 2-DialogKutusu/src/pkg2/dialogkutusu/DialogKutusu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2.dialogkutusu;
+
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author porta
+ */
+public class DialogKutusu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String input =  JOptionPane.showInputDialog("İnput Girin");
+        System.out.println(input);
+    }
+    
+}
