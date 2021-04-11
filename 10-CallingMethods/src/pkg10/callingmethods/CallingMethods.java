53fc172c9e9aca7593c58d0124384fcd8c2f30ae
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-04-11 10:28:23 +0300

    10-CallingMethods

---
+++ 10-CallingMethods/src/pkg10/callingmethods/CallingMethods.java
@@ -0,0 +1,30 @@
+
+package pkg10.callingmethods;
+
+public class CallingMethods {
+
+    public static int max(int num1,int num2)
+    {
+        int result;
+        
+        if(num1 > num2)
+            result = num1;
+        else
+            result = num2;
+        return result;
+    
+    
+    }
+    
+    
+    public static void main(String[] args) {
+     int i=5;
+     int j=2;
+     int k= max(i, j);
+     
+     System.out.println(k);
+        
+        
+    }
+    
+}
