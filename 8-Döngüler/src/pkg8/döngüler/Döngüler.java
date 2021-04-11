0b5fbc319f0cf076d6108aac91f16802809c67b1
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-04-11 10:26:16 +0300

    8-Döngüler

---
+++ 8-Döngüler/src/pkg8/döngüler/Döngüler.java
@@ -0,0 +1,20 @@
+
+package pkg8.döngüler;
+
+
+public class Döngüler {
+
+ 
+    public static void main(String[] args) {
+    
+        int a=0;
+        while (a<100)
+        {
+        System.out.println("Javaya Hoşgeldiniz");
+        a++;
+        
+        }
+    
+    }
+    
+}
