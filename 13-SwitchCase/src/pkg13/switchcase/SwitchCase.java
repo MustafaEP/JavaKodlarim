8763f60ad3008b70ce816f87b3a73b02dafed50c
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-05-01 18:48:02 +0300

    13-SwitchCase

---
+++ 13-SwitchCase/src/pkg13/switchcase/SwitchCase.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg13.switchcase;
+
+/**
+ *
+ * @author porta
+ */
+public class SwitchCase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int i=2;
+        switch(17)
+        {
+            case 1:
+                System.out.println("Sayı 1");
+                break;
+                
+            case 2:
+                System.out.println("Sayı 2");
+                break;
+                
+            case 3:
+                System.out.println("Sayı 3");    
+                break;
+                
+            default:
+                System.out.println("Sayı Bulunamadı");
+                break;
+                
+        }
+        
+        
+    }
+    
+}
