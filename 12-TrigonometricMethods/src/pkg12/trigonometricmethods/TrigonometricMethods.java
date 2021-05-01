9e632f18bc0d1c914768adcd34dee47c7c3482d0
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-05-01 18:47:04 +0300

    12-TrigonometricMethods

---
+++ 12-TrigonometricMethods/src/pkg12/trigonometricmethods/TrigonometricMethods.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg12.trigonometricmethods;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author porta
+ */
+public class TrigonometricMethods {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bir AÇı Giriniz: ");
+        Scanner scanner = new Scanner(System.in);
+        double a;
+        a = scanner.nextDouble();
+        
+        System.out.println("");
+        
+    }
+    
+}
