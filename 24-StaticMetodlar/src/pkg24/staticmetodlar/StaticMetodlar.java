24fb0ea3057b1b5b898e00b18b8edaad263972c2
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-07-05 09:36:58 +0300

    24-StaticMetodlar

---
+++ 24-StaticMetodlar/src/pkg24/staticmetodlar/StaticMetodlar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg24.staticmetodlar;
+
+/**
+ *
+ * @author porta
+ */
+public class StaticMetodlar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=5, b=7;
+        
+        System.out.println((int)Math.pow(a,b));
+        //Math.pow(a,b)=a üzeri b dir ve bir static metodtur.        
+    }
+    
+}
