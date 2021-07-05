bace1cf89942e661a14af22683221bc33f2e61be
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-07-05 09:38:41 +0300

    26-AnahtarKelimeler

---
+++ 26-AnahtarKelimeler/src/pkg26/anahtarkelimeler/AnahtarKelimeler.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg26.anahtarkelimeler;
+
+
+class sınıf{
+    
+    static int x=20;
+    static int y=25;
+    static void yazdır()
+    {
+        System.out.println("x: " + x + "y: " + y);
+    }
+    
+}
+
+
+ class AnahtarKelimeler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       sınıf.yazdır();
+    }
+    
+}
