e9501232f38527e743df6b19d63b69cb95a05623
Mustafa Erhan Portakal <73031908+MustafaEP@users.noreply.github.com>
2021-07-19 10:12:09 +0300

    28-Diziler

---
+++ 28-Diziler/src/pkg28/diziler/Diziler.java
@@ -0,0 +1,34 @@
+
+
+
+package pkg28.diziler;
+
+
+
+
+public class Diziler {
+
+    
+    
+    
+    public static void main(String[] args) {
+        
+        int[] dizi = {1,3,4,5,6,7,432,2435,5};
+        String[] dizi1 = {"Mehmet","Erhan"};
+        
+        int [] dizi2 = new int[10];
+        
+        int dizi3[];
+        dizi3 = new int[]{1,3,4,6,'3'};
+        
+        Object dizi4 [] = new Object[]{1, 4, '4', "Galatasaray"};
+        
+        System.out.println(dizi[0] + " " + dizi3[4] + dizi1[0] + dizi4[3]);
+        
+        System.out.println(dizi4.length); //length özelliği dizinin boyutunu gösterir.
+        
+        
+        
+    }
+    
+}
